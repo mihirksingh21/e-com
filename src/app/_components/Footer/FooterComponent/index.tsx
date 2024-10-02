521a4185e140e8c6cd4a4d4f1a728dbb3e29787c
mihir21 <92274883+mihirksingh21@users.noreply.github.com>
2024-10-02 12:49:09 +0530

    .

---
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import classes from './index.module.scss'
+const FooterComponent = () => {
+  return (
+    <footer>
+      
+    </footer>
+  )
+}
+
+export default FooterComponent
