dac90ca241041dab56349146814496da528c5d30
mihir21 <92274883+mihirksingh21@users.noreply.github.com>
2024-10-07 13:36:11 +0530

    fixed linting

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -16,6 +16,7 @@ import { Gutter } from '../../_components/Gutter'
 export const dynamic = 'force-dynamic'
 
 import classes from './index.module.scss'
+import Categories from '../../_components/Categories'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
@@ -51,7 +52,10 @@ export default async function Page({ params: { slug = 'home' } }) {
         <section>
           <Hero {...hero} />
 
-          <Gutter>{/*categories*/}</Gutter>
+          <Gutter className={classes.home}>
+            <Categories categories={categories} />
+           
+          </Gutter>
           </section>
         
       ) : (
