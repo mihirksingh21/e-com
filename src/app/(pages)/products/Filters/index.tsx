335cb1a1acc273ba1c9896fa6e9c5215e931ba68
mihir21 <singhmihir466@gmail.com>
2024-10-17 15:25:52 +0530

    Fixed categories and sort functioning

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,42 +1,48 @@
 'use client'
 
-import React from 'react';
-import { Category } from '../../../../payload/payload-types';
-import { Checkbox } from '../../../_components/Checkbox';
-import { HR } from '../../../_components/HR';
-import { RadioButton } from '../../../_components/Radio';
-import { useFilter } from '../../../_providers/Filter';
+import React from 'react'
 
-import classes from './index.module.scss';
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from '../../../_components/Checkbox'
+import { HR } from '../../../_components/HR'
+import { RadioButton } from '../../../_components/Radio'
+import { useFilter } from '../../../_providers/Filter'
+
+import classes from './index.module.scss'
 
 const Filters = ({ categories }: { categories: Category[] }) => {
-  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter();
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
 
   const handleCategories = (categoryId: string) => {
     if (categoryFilters.includes(categoryId)) {
-      const updatedCategories = categoryFilters.filter(id => id !== categoryId);
-      setCategoryFilters(updatedCategories);
-    } else {
-      setCategoryFilters([...categoryFilters, categoryId]);
-    }
-  };
+      const updatedCategories = categoryFilters.filter(id => id !== categoryId)
 
-  const handleSort = (value: string) => setSort(value);
+      setCategoryFilters(updatedCategories)
+    } else {
+      setCategoryFilters([...categoryFilters, categoryId])
+    }
+  }
+
+  const handleSort = (value: string) => setSort(value)
 
   return (
     <div className={classes.filters}>
       <div>
         <h6 className={classes.title}>Product Categories</h6>
         <div className={classes.categories}>
-          {categories.map(category => (
-            <Checkbox
-              key={category.id}
-              label={category.title}
-              value={category.id}
-              isSelected={categoryFilters.includes(category.id)}
-              onClickHandler={handleCategories}
-            />
-          ))}
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
+
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
+          })}
         </div>
         <HR className={classes.hr} />
         <h6 className={classes.title}>Sort By</h6>
@@ -58,7 +64,7 @@ const Filters = ({ categories }: { categories: Category[] }) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Filters;
+export default Filters
--- src/app/_components/Radio/index.tsx
+++ src/app/_components/Radio/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-
 import classes from './index.module.scss'
-
 interface RadioButtonProps {
   label: string
   value: string
